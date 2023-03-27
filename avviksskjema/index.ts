bee23f1af6e6914ce5f608daad52bc39e33aefa6
Stian Grepperud <stian.grepperud@puzzlepart.com>
2023-03-27 14:10:19 +0200

    Support for new API endpoint to fetch a case by casenumber

--- avviksskjema/index.ts
+++ avviksskjema/index.ts
@@ -10,15 +10,32 @@ const getToken = async function () {
     params.append('password', process.env.TOKEN_PASSWORD);
     const response = await axios.post(
         process.env.TOKEN_URL, params,
-        { headers: {
-            'X-PrettyPrint': '1',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        } },
+        {
+            headers: {
+                'X-PrettyPrint': '1',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        },
     );
     return response.data['access_token'];
 }
 
+const getRequestURI = function (mode: string) {
+    let uri = process.env.API_BASE_URL;
+    switch (mode) {
+        case 'post':
+            uri += 'Avviksskjema'
+        case 'get':
+            uri += 'avvik/getCase'
+        default:
+            break;
+    }
+
+    return uri;
+}
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const mode = req.query.mode
     try {
         // Get token
         const access_token = await getToken();
@@ -33,12 +50,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${access_token}`
                 };
-                const result = await axios.post(process.env.FORM_URL, data, {headers});
-                context.res = {
-                    status: result.status,
-                    body: result.data,
-
-                };
+                const url = getRequestURI(mode);
+                if (url !== "") {
+                    const result = await axios.post(url, data, { headers });
+                    context.res = {
+                        status: result.status,
+                        body: result.data,
+                    };
+                } else {
+                    context.res = {
+                        status: 400,
+                        body: "Missing or invalid parameter: 'mode'"
+                    };
+                }
 
             } catch (error) {
                 // Try to return error as is from Salesforce (response statuses for errors seem to always be 500)
